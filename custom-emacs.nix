0d0d04fa69f6860a5e9252d50175ccfd37c084a4
Soham S Gumaste <sgumas2@uic.edu>
2025-02-20 14:02:03 -0600

    bump

--- custom-emacs.nix
+++ custom-emacs.nix
@@ -10,9 +10,4 @@ emacs-pgtk.overrideAttrs (old: {
   stdenv = ccacheStdenv;
   NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + "-O3 -march=native -mtune=native";
 
-  withXwidgets = true;
-
-  buildInputs = old.buildInputs ++ [ webkitgtk_4_0.dev ];
-  nativeBuildInputs = old.nativeBuildInputs ++ [ wrapGAppsHook3 webkitgtk_4_0.dev ];
-  configureFlags = (lib.lists.remove "--without-xwidgets" old.configureFlags) ++ ["--with-xwidgets"];
 })
