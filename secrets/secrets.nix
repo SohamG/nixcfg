1a770fc48d7e0ded989aa2af8ae955348e6f7977
Soham S Gumaste <sgumas2@uic.edu>
2025-04-05 11:22:56 -0500

    Nebulized

---
+++ secrets/secrets.nix
@@ -0,0 +1,13 @@
+let
+  thonker = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKTdM+j357oG4+58wUtPp60mo4j0IZ90RUFCQVELJ5YX root@thonker";
+  yamlmaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOm7cVyjZVTQJZ4dvMWEnKPjOmuBusRfhRGXuHrJgl8E";
+in
+{
+  "nebula-key.age".publicKeys = [ thonker ];
+  "nebula-crt.age".publicKeys = [ thonker ];
+  "nebula-ca.age".publicKeys = [ thonker ];
+
+  "nebula-yamlmaster-key.age".publicKeys = [ yamlmaster ];
+  "nebula-yamlmaster-crt.age".publicKeys = [ yamlmaster ];
+  "nebula-yamlmaster-ca.age".publicKeys= [ yamlmaster ];
+}
---
+++ yamlmaster/yamlmaster.nix
@@ -0,0 +1,214 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./yamlmaster-hw.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "yamlmaster"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+  age.secrets = {
+    nebula-key = {
+      file = ../secrets/nebula-yamlmaster-key.age;
+      path = "/etc/nebula-host.key";
+      owner = "nebula-mesh";
+      group = "nebula-mesh";
+      mode = "750";
+    };
+
+    nebula-crt = {
+      file = ../secrets/nebula-yamlmaster-crt.age;
+      path = "/etc/nebula-host.crt";
+      owner = "nebula-mesh";
+      group = "nebula-mesh";
+      mode = "750";
+    };
+
+    nebula-ca = {
+      file = ../secrets/nebula-yamlmaster-ca.age;
+      path = "/etc/nebula-ca.crt";
+      owner = "nebula-mesh";
+      group = "nebula-mesh";
+      mode = "750";
+    };
+  };
+
+  services.nebula.networks.mesh = {
+    ca = config.age.secrets.nebula-ca.path;
+    cert = config.age.secrets.nebula-crt.path;
+    key = config.age.secrets.nebula-key.path;
+
+    enable = true;
+
+    settings.punchy = {
+      punch = true;
+      respond = true;
+    };
+    firewall.inbound = [
+      {
+        host = "any";
+        port = "any";
+        proto = "any";
+      }
+    ];
+
+    firewall.outbound = [
+      {
+        host = "any";
+        port = "any";
+        proto = "any";
+      }
+    ];
+
+    isLighthouse = false;
+    staticHostMap = {
+      "192.168.0.100" = [ "sohamg.xyz:4242" ];
+    };
+    lighthouses = [ "192.168.0.100" ];
+  };
+
+
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = false;
+  services.desktopManager.plasma6.enable = false;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = false;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.user = {
+    isNormalUser = true;
+    description = "user";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      # kdePackages.kate
+    #  thunderbird
+      neovim
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = false;
+  services.xserver.displayManager.autoLogin.user = false;
+
+  # Install firefox.
+  programs.firefox.enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    neovim
+    bottom
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
